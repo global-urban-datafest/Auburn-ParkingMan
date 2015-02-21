0184dfc1f36a2f360d6a3c6b55fa1f220f195c5c
Richard Kosbab <rombus@gmail.com>
2015-02-21 16:18:39 -0600

    Commit to show CoreData to View connectivity. NOTE: Current bug is that there is no database cleanup, will continue to grow Coredata DB.

--- ParkingMan/ParkingMan/ViewController.swift
+++ ParkingMan/ParkingMan/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import CoreData
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    var spots = [CityOfAuburn]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "List of Auburn Spots"
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         AuburnImport.get()
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return spots.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+            let spot = spots[indexPath.row]
+            cell.textLabel!.text = spot.stallnumber.stringValue
+            return cell
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        println("ViewWillAppear")
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        let fetchrequest = NSFetchRequest(entityName: "CityOfAuburn")
+        
+        var error: NSError?
+        
+        let fetchedResults = managedContext.executeFetchRequest(fetchrequest, error: &error) as [CityOfAuburn]?
+        
+        if let results = fetchedResults {
+            println(results.count)
+            spots = results
+            self.tableView.reloadData()
+        }
     }
 
 
